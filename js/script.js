5ce1e17a5985922a041322b66974edff5c7271e7
Michal Barczynski <barczynski_sggw@wp.pl>
2022-03-11 16:48:18 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+/* * * * 1 * * * */
+
+const names = ['Kasia', 'Tomek', 'Amanda', 'Maja'];
+let femaleNames = [];
+
+for (let name of names) {
+  let femaleName = '';
+  if (name.charAt(name.length) = "a") {
+    femaleNames.push(femaleName);
+  }
+}
+
+
+/* * * * 2 * * * */
+const employees = {
+  john: {
+    name: 'John Doe',
+    salary: 3000
+  },
+  amanda: {
+    name: 'Amanda Doe',
+    salary: 4000
+  },
+}
+
+
+const employeesNames = [];
+const employeesSalaries  = [];
+
+for (let data of employees) {
+  employeesNames = data.split(data.name);
+  employeesSalaries = data.split(data.salary);
+}
+
+
+/* * * * 3 * * * */
+const salaries = [2000, 3000, 1500, 6000, 3000];
+const exampleSalary = salaries[0];
+
+for (let maxSalary of salaries) {
+  
+}
+
+
+/* * * * 4 * * * */
+const persons = {
+  john: 2000,
+  amanda: 3000,
+  thomas: 1500,
+  james: 6000,
+  claire: 3000
+};
+
+
+
+
+
+
+/* * * * 5 * * * */
+
+const tags = ['news', 'code', 'news', 'sport', 'hot', 'news', 'code'];
+
+const result = {
+  news: { appearances: 3 },
+  code: { appearances: 2 },
+  sport: { appearances: 1 },
+  hot: { appearances: 1 },
+}
